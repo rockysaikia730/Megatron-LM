19a51531bdccc9e83850070299b2be36e935e646
Guanshujie Fu <fuguan@student.ethz.ch>
2026-03-14 20:46:51 +0100

    fix: OOM w/ alps3 image and ckpt loading

--- megatron/core/energy_monitor.py
+++ megatron/core/energy_monitor.py
@@ -58,6 +58,8 @@ class EnergyMonitor:
 
     def _get_energy(self) -> int:
         """Get current energy consumption from NVML."""
+        # NOTE (fuguan): with alps3 image, OOM error may occur when calling nvmlDeviceGetTotalEnergyConsumption
+        return self._last_energy  # return *something* if it errors
         try:
             return nvmlDeviceGetTotalEnergyConsumption(self._handle)
         except NVMLError:
--- megatron/training/checkpointing.py
+++ megatron/training/checkpointing.py
@@ -19,6 +19,7 @@ import numpy as np
 import torch
 from typing import Optional, Union, List, Dict, Any
 from torch.distributed.checkpoint import FileSystemReader, default_planner
+import gc
 
 from megatron.core import dist_checkpointing, mpu, tensor_parallel
 from megatron.core.dist_checkpointing.mapping import ShardedObject
@@ -1664,6 +1665,11 @@ def load_checkpoint(ddp_model, optimizer, opt_param_scheduler, load_arg='load', 
             load_dir, args, rank0=False, checkpointing_context=checkpointing_context,
             **load_kwargs
         )
+    del load_kwargs
+
+    # collect the freed reference to load_kwargs so that CUDA can let go of load_kwargs["sharded_state_dict"] 
+    gc.collect()
+    torch.cuda.empty_cache()
 
     # Checkpoint not loaded.
     if state_dict is None:
@@ -1722,6 +1728,7 @@ def load_checkpoint(ddp_model, optimizer, opt_param_scheduler, load_arg='load', 
     if not skip_load_to_model_and_opt:
         if len(ddp_model) == 1:
             load_model_state_dict(ddp_model[0], state_dict['model'], strict)
+            del state_dict['model']
         else:
             for i in range(len(ddp_model)):
                 # If there is no corresponding model in the state_dict, it will be ignored.
@@ -1729,6 +1736,7 @@ def load_checkpoint(ddp_model, optimizer, opt_param_scheduler, load_arg='load', 
                 if 'model%d' % i not in state_dict:
                     continue
                 load_model_state_dict(ddp_model[i], state_dict['model%d' % i], strict)
+                del state_dict['model%d' % i]
     # Fix up query/key/value matrix ordering if needed.
     checkpoint_version = get_checkpoint_version()
     print_rank_0(f' checkpoint version {checkpoint_version}')
@@ -1740,6 +1748,7 @@ def load_checkpoint(ddp_model, optimizer, opt_param_scheduler, load_arg='load', 
             # Load state dict.
             if not skip_load_to_model_and_opt and optimizer is not None and not optimizer.is_stub_optimizer:
                 optimizer.load_state_dict(state_dict['optimizer'])
+                del state_dict['optimizer']
 
             # Load distributed optimizer's custom parameter state.
             # For distributed checkpoint it's already loaded in load_state_dict above
@@ -1856,6 +1865,8 @@ def load_checkpoint(ddp_model, optimizer, opt_param_scheduler, load_arg='load', 
        or is_last_rank():
         wandb_utils.on_load_checkpoint_success(checkpoint_name, load_dir)
 
+    # collect freed references to state_dict['model'] and state_dict['optimizer'] so CUDA can let go of the memory
+    gc.collect()
     torch.cuda.empty_cache()
 
     if iteration > 0:
